9ca06c35c9c6e46a2e6bba3e457c62a958d17edf
Mauro Bringolf <maurob@student.ethz.ch>
2021-09-14 21:39:57 +0200

    Change License header of modified files.

--- tests/integration/misc/FileSizeLimit.py
+++ tests/integration/misc/FileSizeLimit.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) [2020 - 2021] The yinyang authors
+# Copyright (c) [2020 - 2021] Mauro Bringolf and Dominik Winterer
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
