40f53902784482d91c48570657faffe22f17ebd6
Mauro Bringolf <maurob@student.ethz.ch>
2021-09-20 22:12:03 +0200

    Minor improvements

--- scripts/browse-reduced-bugs.sh
+++ scripts/browse-reduced-bugs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for f in reduced/regression-incompleteness*.smt2; do
+for f in $(/bin/ls -rS -1 reduced/regression-incompleteness*.smt2); do
 	echo "REGRESSION: $f"
 	cat $f
 	echo "-------------------------"
