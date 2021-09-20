85255f1e117e972c27df02d32880d8db8123454a
Mauro Bringolf <maurob@student.ethz.ch>
2021-09-20 22:34:34 +0200

    Change config to stub config

--- fuzzing_configuration.py
+++ fuzzing_configuration.py
@@ -1,16 +1,29 @@
-Z3_NEW = '/local/home/maurob/impbased-fuzzing-experiments/solvers/z3/build/z3 -smt2'
-Z3_OLD = '/local/home/maurob/impbased-fuzzing-experiments/solvers/z3-4.8.12/build/z3 -smt2'
-CVC5_NEW = '/local/home/maurob/impbased-fuzzing-experiments/solvers/cvc4/build/bin/cvc5 -q --strings-exp --lang=smt2'
-CVC5_OLD = '/local/home/maurob/impbased-fuzzing-experiments/solvers/cvc4-1.8 -q --strings-exp --lang=smt2'
+# Solver commands
+Z3_NEW = 'PATH_TO_Z3_NEW -smt2'
+Z3_OLD = 'PATH_TO_Z3_OLD -smt2'
+CVC5_NEW = 'PATH_TO_CVC5_NEW -q --strings-exp --lang=smt2'
+CVC5_OLD = 'PATH_TO_CVC5_OLD -q --strings-exp --lang=smt2'
 
-SEED_DIR = '/local/home/maurob/impbased-fuzzing-experiments/semantic-fusion-seeds/'
+# Path to seeds directory, can have any hierarchy inside.
+# We try to determine satisfiability based on the path, e.g. 'SEED_DIR/.../sat/.../file.smt2 will be labelled as satisfiable.
+SEED_DIR = ''
+
+# Each instance will run at most one solver as subprocess at any time,
+# so this option roughly produces in 2*MAX_NUM_INSTANCES processes.
 MAX_NUM_INSTANCES = 40
+
+# This produces roughly MAX_NUM_REDUCERS*NUM_PROCS_PER_REDUCER processes.
 MAX_NUM_REDUCERS = 5
 NUM_PROCS_PER_REDUCER = 10
-SOLVER_TIMEOUT = 10
+
+# Janus parameters
 ITERATIONS = 300
 WALK_LENGTH = 20
+RULE_SET = 'all'
+
+# Solver timeout used for fuzzing and reducing
+SOLVER_TIMEOUT = 10
 TIMEOUT_PER_INSTANCE = f"{str(ITERATIONS*SOLVER_TIMEOUT)}s"
 TIMEOUT_PER_REDUCER = '1h'
-RULE_SET = 'all'
-OUTPUT_WIDTH = 80
+
+
