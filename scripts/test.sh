608b9625361c3e50ce0af70c1ba7eab75eaba91a
wintered <domwinterer@gmail.com>
2022-10-10 17:50:13 +0200

    interim

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,9 +4,21 @@ set -e
 
 ./scripts/black-check.sh
 python3 -m unittest tests/RunUnitTests.py
+
+echo python3 tests/integration/detection/TestDetection.py
 python3 tests/integration/detection/TestDetection.py
+
+echo python3 tests/integration/detection/TestCrashes.py
 python3 tests/integration/detection/TestCrashes.py
+
+echo python3 tests/integration/old-issues/TestIssueRediscovery.py
 python3 tests/integration/old-issues/TestIssueRediscovery.py
+
+echo python3 tests/integration/misc/FileSizeLimit.py
 python3 tests/integration/misc/FileSizeLimit.py
+
+echo python3 tests/integration/misc/NoSolvers.py
 python3 tests/integration/misc/NoSolvers.py
+
+echo python3 tests/integration/misc/DirectoryMode.py
 python3 tests/integration/misc/DirectoryMode.py
